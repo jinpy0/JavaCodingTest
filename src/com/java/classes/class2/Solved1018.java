a929acb3ece0ff603a21234a3178df529c98673d
jinpy0 <jinpyo0920@gmail.com>
2025-01-09 16:30:11 +0900

    Solved1018

--- src/com/java/classes/class2/Solved1018.java
+++ src/com/java/classes/class2/Solved1018.java
@@ -1,7 +1,30 @@
 package com.java.classes.class2;
 
-public class Solved1018 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Solved1018 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        String[][] arr = new String[n][m];
+
+        for (int j = 0; j < n; j++) {
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < m; i++) { // x 좌표만 이동
+                arr[i][j] = st.nextToken();
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
